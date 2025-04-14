35df99bbb8f94f639746fb39e1af50340c8fefca
TRAHIM86 <trahim1986@gmail.com>
2025-04-14 20:42:04 +0300

    feat: completed task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,8 +92,13 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...rest) {
+  return function poly(x) {
+    const res = rest.reduce((acc, arg, index) => {
+      return acc + arg * x ** (rest.length - index - 1);
+    }, 0);
+    return res;
+  };
 }
 
 /**
